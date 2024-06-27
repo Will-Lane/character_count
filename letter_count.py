2f4d6f0f3c65e7bd141926caeca52c2a91d49663
William Lane <willjlane@hotmail.co.uk>
2024-06-27 21:17:44 +0100

    by starting letter

--- letter_count.py
+++ letter_count.py
@@ -12,32 +12,32 @@ def get_words(a, b):
         words_4_5_6 = ([i.upper() for i in all_words if len(i)>= a and len(i) <= b])
         return words_4_5_6
     
-words = get_words(min_length,max_length)
-words.sort()
-print(words)
+words_list = get_words(min_length,max_length)
+# print(words)
 
 alpha = string.ascii_uppercase
 
-print(alpha)
+# print(alpha)
+for start_letter in alpha:
+    print(start_letter)
+    words = [x for x in words_list if x.startswith(start_letter)]
+
+    for wordlen in range(min_length, max_length+1):
+        print(f'Count for {wordlen} letter words!')
+        worddict = {idx : {letter : set() for letter in alpha} for idx in range(wordlen)}
+        for word in words:
+            if len(word) == wordlen and all(letter in alpha for letter in word):
+                for idx, letter in enumerate(word):
+                    worddict[idx][letter].add(word)
+
+        lens = [[(len(worddict[idx][letter]), idx, letter) for letter in worddict[idx]] for idx in worddict]
 
 
-for wordlen in range(min_length, max_length+1):
-    print(f'Count for {wordlen} letter words!')
-    worddict = {idx : {letter : set() for letter in alpha} for idx in range(wordlen)}
-
-    for word in words:
-        if len(word) == wordlen and all(letter in alpha for letter in word):
-            for idx, letter in enumerate(word):
-                worddict[idx][letter].add(word)
-
-    lens = [[(len(worddict[idx][letter]), idx, letter) for letter in worddict[idx]] for idx in worddict]
-
-
-    for item in lens:
-        sort = sorted(item,key=itemgetter(0), reverse=True)
-        top_5 = sort[:5]
-        # print(top_5)
-        rank=1
-        for num, idx, letter in top_5:
-            print(f"Position: {idx+1}    Letter: {letter}    Total: {num}    Rank: {rank}")
-            rank+=1
+        for item in lens:
+            sort = sorted(item,key=itemgetter(0), reverse=True)
+            top_5 = sort[:5]
+            # print(top_5)
+            rank=1
+            for num, idx, letter in top_5:
+                print(f"Starting letter: {start_letter}    Position: {idx+1}    Letter: {letter}    Total: {num}    Rank: {rank}")
+                rank+=1
